4e2846667528c4d5fe0e688e357bcafa2ff470ae
omoidesu <949395971@qq.com>
2022-08-04 04:45:33 +0800

    queue length in ls card

--- app/CardStorage.py
+++ app/CardStorage.py
@@ -153,7 +153,7 @@ def MusicListCard(music_list: list[Music]) -> Tuple[Card, Card]:
         )
         if len(remaining_list_card._modules) >= 46:
             remaining_list_card.append(Module.Divider())
-            remaining_list_card.append(Module.Header('・・・'))
+            remaining_list_card.append(Module.Header(f'str({len(music_list)-16}) 首音乐被折叠'))
             break
 
     return NowMusicCard(music_list), remaining_list_card
