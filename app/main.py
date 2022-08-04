34ebcbb1573f3029d68878ec57391afb69311e52
omoidesu <949395971@qq.com>
2022-08-04 13:14:23 +0800

    requirements

--- app/main.py
+++ app/main.py
@@ -632,7 +632,7 @@ async def startup_tasks():
 
 
 # repeated tasks
-@bot.task.add_interval(seconds=5)
+@bot.task.add_interval(seconds=1)
 async def five_seconds_interval_tasks():
     await update_played_time_and_change_music()
 
